d924df3715d276886f05132ae853a211336bd257
Ahmedshk <obaid3035@gmail.com>
2022-06-06 18:32:38 +0500

    all done

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -5,46 +5,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-// export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit {
 
-//   constructor() {
+  constructor() {
 
-//    }
+   }
 
-//   ngOnInit(): void {
-//   }
-
-// }
-
-export class HomeComponent {
-  slides = [
-    { img: "http://placehold.it/350x150/000000" },
-    { img: "http://placehold.it/350x150/111111" },
-    { img: "http://placehold.it/350x150/333333" },
-  ];
-  slideConfig = { "slidesToShow": 1, "slidesToScroll": 1, "dots": true };
-
-  addSlide() {
-    this.slides.push({ img: "http://placehold.it/350x150/777777" })
+  ngOnInit(): void {
   }
 
-  removeSlide() {
-    this.slides.length = this.slides.length - 1;
-  }
-
-  slickInit() {
-    console.log('slick initialized');
-  }
-
-  breakpoint() {
-    console.log('breakpoint');
-  }
-
-  afterChange() {
-    console.log('afterChange');
-  }
-
-  beforeChange() {
-    console.log('beforeChange');
-  }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { HomeComponent } from './Pages/home/home.component';
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: AppComponent}
 ];
 
 @NgModule({
